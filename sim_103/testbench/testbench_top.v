352a8e63b55f65194aa8b55db85e014128344195
sim_pr <wan.work@foxmail.com>
2023-02-03 12:14:33 +0800

    使用iverilog编译仿真，生成波形文件

--- sim_103/testbench/testbench_top.v
+++ sim_103/testbench/testbench_top.v
@@ -54,6 +54,10 @@ initial begin
 	$stop;
 end
 
+initial begin
+	$dumpfile("./build/wave.vcd");  // 指定VCD文件的名字为wave.vcd，仿真信息将记录到此文件
+	$dumpvars(0, testbench_top );  // 指定层次数为0，则testbench_top 模块及其下面各层次的所有信号将被记录
+end
 
 endmodule
 
